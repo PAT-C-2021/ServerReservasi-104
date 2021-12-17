1c1d0a893f0336e475b5f5401cc753904101dfb1
Eka Nova Pramudya <78529450+pramek008@users.noreply.github.com>
2021-12-17 16:57:56 +0700

    Add project files.

---
+++ ServerConfigReservasi/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceReservasi;
+
+namespace ServerConfigReservasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost serviceHost = null;
+            try {
+                serviceHost = new ServiceHost(typeof(Service1));
+                serviceHost.Open();
+                Console.WriteLine("Server is Ready !!!");
+                Console.ReadLine();
+                serviceHost.Close();
+            }
+            catch(Exception e) { 
+                serviceHost = null;
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
